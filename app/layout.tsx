1fdebe3d6e54521ec831ab321d9686e19eb77b70
kazaikstudio <kazaikstudioz@gmail.com>
2026-09-03 21:38:36 +0300

    Use NollPage SVG favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
     "Noll Studios (NSU) - Your description here. Discover our work and services.",
   metadataBase: new URL("https://nollstudios.org"),
   icons: {
-    icon: "/favicon.ico",
+    icon: "/NollPage.svg",
   },
   openGraph: {
     title: "Noll Studios - NSU | Creative Studio",
